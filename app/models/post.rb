21aced7b220f652493417f03c561d74246999b96
Joanna <astronautnemo@gmail.com>
2023-03-16 20:30:18 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :is_clickbaity
+
+    # Custom validations
+    def is_clickbaity
+        # byebug
+        if [/Won't Believe/i, /Secret/i, /Top/i, /Guess/i].none?{|clickbait| clickbait.match title}
+            errors.add(:title, "Not clickbait-y enough!")
+        end
+    end
 end
